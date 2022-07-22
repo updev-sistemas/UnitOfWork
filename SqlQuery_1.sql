aff00335f60876c0981a5345d2525178436bf115
Antonio José <falecomoantonio@live.com>
2022-07-22 20:54:28 -0300

    add sql file.

---
+++ SqlQuery_1.sql
@@ -0,0 +1,11 @@
+﻿create database poc_unit_of_work;
+
+use poc_unit_of_work;
+
+CREATE TABLE poc_unit_of_work.dbo.tb_users (
+	id bigint IDENTITY(1,1) NOT NULL,
+	email varchar(120) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	username varchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	created_at datetime2(0) NULL,
+	updated_at datetime2(0) NULL
+);
